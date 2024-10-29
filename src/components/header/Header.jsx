d44fcdf9a669eee59f785830bcd4fd2c49d366f3
pitbull90 <henrycordero@live.com>
2024-10-28 20:23:03 -0700

    Finished the nav and the scroll effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { links } from '../../Data';
 import { FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
 import { BsSun, BsMoon } from 'react-icons/bs';
 import './header.css';
+import { Link } from 'react-scroll';
 
 const Header = () => {
+    const [ showMenu, setShowMenu ] = useState(false);
+
+    useEffect(() => {
+        document.body.classList.toggle('no-scroll', showMenu);
+    }, [showMenu]);
+
   return (
     <header className="header">
         <nav className="nav">
             <a href="" className="nav__logo text-cs">Henry</a>
 
-            <div className="nav__menu">
+            <div className={`${showMenu ? 'nav__menu show_menu' : 'nav__menu'}`}>
                 <div className="nav__data">
                     <ul className="nav__list">
                         {links.map(({ name, path }, index) => {
                             return (
                                 <li key={index} className="nav__item">
-                                    <a href='' className="nav__link text-cs">{name}</a>
+                                    <Link
+                                        className='nav__link text-cs'
+                                        to={path}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        onClick={() => setShowMenu(!showMenu)}
+                                    >
+                                        {name}
+                                    </Link>
                                 </li>
                             )
                         })}
@@ -41,7 +58,7 @@ const Header = () => {
                     <BsSun />
                 </div>
 
-                <div className="nav__toggle">
+                <div className={`${showMenu ? 'nav__toggle animate_toggle' : 'nav__toggle '}`} onClick={() => setShowMenu(!showMenu)}>
                     <span></span>
                     <span></span>
                 </div>
