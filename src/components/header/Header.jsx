76fcfbd747818fc285c196eb919ff560e338dc11
pitbull90 <henrycordero@live.com>
2024-10-30 18:19:01 -0700

    Finished the nav and also completed the theme toggler

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,58 @@ import { FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
 import { BsSun, BsMoon } from 'react-icons/bs';
 import './header.css';
 import { Link } from 'react-scroll';
+import { animateScroll } from 'react-scroll';
+
+const getStorageTheme = () => {
+    let theme = 'light_theme';
+    if (localStorage.getItem('theme')) {
+        theme = localStorage.getItem('theme');
+    }
+}
 
 const Header = () => {
     const [ showMenu, setShowMenu ] = useState(false);
+    const [ scrollNav, setScrollNav ] = useState(false);
+    const [ theme, setTheme ] = useState(getStorageTheme());
+
+    const scrollTop = () => {
+        animateScroll.scrollToTop();
+    };
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+            setScrollNav(true)
+        }
+        else {
+            setScrollNav(false)
+        }
+    }
+
+    const toggleTheme = () => {
+        if (theme === 'light_theme') {
+            setTheme('dark_theme')
+        } else {
+            setTheme('light_theme')
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, []);
 
     useEffect(() => {
         document.body.classList.toggle('no-scroll', showMenu);
     }, [showMenu]);
 
+    useEffect(() => {
+        document.documentElement.className = theme;
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
   return (
-    <header className="header">
+    <header className={`${scrollNav ? 'scroll_header' : ''} header`}>
         <nav className="nav">
-            <a href="" className="nav__logo text-cs">Henry</a>
+            <Link to='/' onClick={scrollTop} className="nav__logo text-cs">Henry</Link>
 
             <div className={`${showMenu ? 'nav__menu show_menu' : 'nav__menu'}`}>
                 <div className="nav__data">
@@ -27,8 +67,9 @@ const Header = () => {
                                         className='nav__link text-cs'
                                         to={path}
                                         spy={true}
+                                        hashSpy={true}
                                         smooth={true}
-                                        offset={50}
+                                        offset={-150}
                                         duration={500}
                                         onClick={() => setShowMenu(!showMenu)}
                                     >
@@ -54,8 +95,8 @@ const Header = () => {
             </div>
 
             <div className="nav__btns">
-                <div className="theme__toggler">
-                    <BsSun />
+                <div className="theme__toggler" onClick={toggleTheme}>
+                    { theme === 'light_theme' ? <BsMoon /> : <BsSun /> }
                 </div>
 
                 <div className={`${showMenu ? 'nav__toggle animate_toggle' : 'nav__toggle '}`} onClick={() => setShowMenu(!showMenu)}>
